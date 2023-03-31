8e23e6fc686a7c40d857e957d1737dbc1f9abd42
Юрій-Юркевич <yurko0510yurko@gmail.com>
2023-03-31 14:48:09 +0300

    HW 10 js done... may be

--- src/function/fetchCountries.js
+++ src/function/fetchCountries.js
@@ -1,8 +1,5 @@
 export function fetchCountries(name) {
   const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-  
-
-  
   return fetch(URL)
     .then(response => {
       return response.json();
